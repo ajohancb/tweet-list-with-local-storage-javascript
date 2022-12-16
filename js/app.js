db2bcc5fcfa4b89f915dcb384741e6f6e56c7fae
ajohancb <119433130+ajohancb@users.noreply.github.com>
2022-12-16 18:23:32 -0500

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,116 @@
+// Variables
+const formulario = document.querySelector('#formulario')
+const listaTweets = document.querySelector('#lista-tweets')
+let tweets = []
+
+// Event listeners
+eventListeners()
+
+function eventListeners() {
+    // Cuando el usuario agrega un nuevo tweet
+    formulario.addEventListener('submit', agregarTweet)
+
+    // Cuando el documento esta listo
+    document.addEventListener('DOMContentLoaded', () => {
+        tweets = JSON.parse(localStorage.getItem('tweets')) || []
+
+        crearHTML()
+    })
+}
+
+// Funciones
+function agregarTweet(e) {
+    e.preventDefault()
+
+    // Textarea donde el usuario escribe
+    const tweet = document.querySelector('#tweet').value
+
+    // Validación
+    if (tweet === '') {
+        mostrarError('Un mensaje no puede ir vacio')
+        return // Evita que se ejecuten más lineas de código
+    }
+
+    const tweetObj = {
+        id: Date.now(),
+        tweet
+    }
+
+    // Añadir al arreglo de tweets
+    tweets = [...tweets, tweetObj]
+
+    // Una vez agregado vamos a crear el HTML
+    crearHTML()
+
+    // Reiniciar HTML
+    formulario.reset()
+}
+
+// Mostrar mensaje de error
+function mostrarError(error) {
+    const mensajeError = document.createElement('p')
+    mensajeError.textContent = error
+    mensajeError.classList.add('error')
+
+    // Insertarlo en el contenido
+    const contenido = document.querySelector('#contenido')
+    contenido.appendChild(mensajeError)
+
+    // Elimina la alerta despues de 3 segundos
+    setTimeout(() => {
+        mensajeError.remove()
+    }, 3000);
+}
+
+// Muestra un listado de los tweets
+function crearHTML() {
+
+    limpiarHTML()
+
+    if (tweets.length > 0) {
+        tweets.forEach(tweet => {
+            // Agregar un botón de eliminar
+            const btnEliminar = document.createElement('a')
+            btnEliminar.classList.add('borrar-tweet')
+            btnEliminar.innerText = 'X'
+
+            // Añadir funcion de eliminar
+            btnEliminar.onclick = () => {
+                borrarTweet(tweet.id)
+            }
+            
+            // Crear html
+            const li = document.createElement('li')
+
+            // añadir el texto
+            li.innerText = tweet.tweet
+
+            // asignar el boton
+            li.appendChild(btnEliminar)
+
+            // insertarlo en el html
+            listaTweets.appendChild(li)
+        })
+    }
+
+    sincronizarStorage()
+}
+
+// Agrega los tweets actuales al LocalStorage
+function sincronizarStorage(){
+    localStorage.setItem('tweets', JSON.stringify(tweets))
+}
+
+// Elimina un tweet
+function borrarTweet(id){
+    tweets = tweets.filter(tweet => tweet.id !== id)
+
+    crearHTML()
+}
+
+// Limpiar el html
+function limpiarHTML(){
+    while(listaTweets.firstChild){
+        listaTweets.removeChild(listaTweets.firstChild)
+    }
+}
